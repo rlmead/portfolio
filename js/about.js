ef5e16836d561e0740595f1dc5109164bf736740
Rebecca Mead <beckyleemead@gmail.com>
2020-09-29 16:41:24 -0400

    closes #25

---
+++ js/about.js
@@ -0,0 +1,12 @@
+const about_blurb = [
+    "my name is Becky, and i'm a newcomer to Lexington KY. i just moved here from Edinburgh, Scotland. i've also lived in Philadelphia PA, and i grew up in Derby CT. i call many places home!",
+    "i've worked a broad variety of jobs, and i can't wait to see what comes next! most recently, i worked as Lead Data Engineer for a company that makes speech technology primarily used in video games. before that, i helped homeless men study and apply for educational and vocational credentials. two decades ago, i got my first job ever as a cashier at Wendy's.",
+    "here are some things that interest and motivate me:",
+    [
+        "sustainability",
+        "design",
+        "science",
+        "creativity",
+        "social cooperation"
+    ]
+]
