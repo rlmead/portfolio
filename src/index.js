1394b611c2e5a125b8f843e39611099e4d27d556
Rebecca Mead <beckyleemead@gmail.com>
2020-12-17 15:47:47 -0500

    apply themestr theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import "./theme_1607434693456.css";
 
 ReactDOM.render(
   <React.StrictMode>
