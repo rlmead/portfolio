374ed5681eeb9164c1d51dede14b829e00e96391
Rebecca Mead <beckyleemead@gmail.com>
2020-12-18 09:41:57 -0500

    header rework

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import NavBar from '../components/Navbar.js';
+import React, { useState } from "react"
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import linksData from "../data/links"
 
-function Header(props) {
-    return (
-        <div className='bg-light pt-4'>
-            <h1 style={{fontSize: '2em'}}>also / and</h1>
-            <NavBar
-            pages={props.pages}
-            currentPage={props.currentPage}
-            setPage={props.setPage}/>
-        </div>
-    );
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar
+        expand="md"
+        light
+        className="navbar-default navbar-light sticky-top text-primary bg-white">
+        <h1
+          style={{ fontSize: "2em", textShadow: "4px 4px 4px #93B5C6" }}>
+          r. l. mead
+        </h1>
+        <NavbarToggler onClick={toggle} />
+        <Collapse
+          isOpen={isOpen}
+          navbar >
+          <Nav style={{ display: "flex", flexFlow: "row nowrap" }} className="ml-auto">
+            {
+              linksData.map((item, index) => {
+                return (
+                  <NavItem
+                    className="text-primary"
+                    key={index}>
+                    <a href={item.href} target='_blank'>
+                      <FontAwesomeIcon icon={item.icon} />
+                    </a>
+                  </NavItem>
+                )
+              })
+            }
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </>
+  );
 }
 
 export default Header;
