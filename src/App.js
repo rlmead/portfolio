d66a97fdcef0d272c1caaf780571904d8edf763e
Rebecca Mead <beckyleemead@gmail.com>
2020-12-20 11:59:37 -0500

    finishing touches

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from "react"
-import { Container, Row } from "reactstrap"
-import Header from "./components/Header"
-import ProjectBox from "./components/ProjectBox"
-import Article from "./components/Article"
-import projectsData from "./data/projects"
-import aboutData from "./data/about"
+import React from "react";
+import { Container, Row } from "reactstrap";
+import Header from "./components/Header";
+import ProjectBox from "./components/ProjectBox";
+import projectsData from "./data/projects";
 
 function App() {
     return (
         <>
             <Header />
             <Container>
-                {/* <Article
-                    data={aboutData}
-                /> */}
                 <Row>
                     {
                         projectsData.map((item, index) =>
