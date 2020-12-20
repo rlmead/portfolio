a79b42822a0f37d374f6fcdf2faf80d95c5bed3a
Rebecca Mead <beckyleemead@gmail.com>
2020-12-20 11:57:53 -0500

    finishing touches

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react"
-import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap"
+import React, { useState } from "react";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, Modal, ModalHeader, ModalBody } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import linksData from "../data/links"
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
+import linksData from "../data/links";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => setModal(!modal);
 
   return (
     <>
@@ -13,8 +17,7 @@ function Header() {
         expand="md"
         light
         className="navbar-default navbar-light sticky-top text-primary bg-secondary">
-        <h1
-          style={{ fontSize: "2em", textShadow: "4px 4px 4px #7BC9CC" }}>
+        <h1>
           r. l. mead
         </h1>
         <NavbarToggler
@@ -24,16 +27,20 @@ function Header() {
           navbar>
           <Nav
             style={{ display: "flex", flexFlow: "row nowrap" }} className="ml-auto">
+            <NavItem>
+              <FontAwesomeIcon
+                icon={faQuestion}
+                onClick={() => toggleModal()} />
+            </NavItem>
             {
               linksData.map((item, index) => {
                 return (
                   <NavItem
-                    className="text-primary"
                     key={index}>
                     <a
                       href={item.href}
                       target='_blank'>
-                      <FontAwesomeIcon size="1.5x" icon={item.icon} />
+                      <FontAwesomeIcon icon={item.icon} />
                     </a>
                   </NavItem>
                 )
@@ -42,6 +49,22 @@ function Header() {
           </Nav>
         </Collapse>
       </Navbar>
+      <div>
+        <Modal isOpen={modal} toggle={toggleModal} className="text-primary">
+          <ModalHeader toggle={toggleModal}><h4>about</h4></ModalHeader>
+          <ModalBody>
+            <p>i'm a data engineer and full-stack developer with lots of interests and creative drive. i love making things, and i love learning.</p>
+            <p>here are some things that inspire me:</p>
+            <ul>
+              <li>sustainability</li>
+              <li>design</li>
+              <li>science</li>
+              <li>creativity</li>
+              <li>social cooperation</li>
+            </ul>
+          </ModalBody>
+        </Modal>
+      </div>
     </>
   );
 }
--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -6,10 +6,9 @@ function ProjectBox(props) {
 
   return (
     <Col
-      md="4"
-      className="px-0">
+      md="4">
       <Card
-        className="border-0 rounded-0"
+        className="border-0 mb-4"
         onMouseOver={() => setHidden(false)}
         onMouseOut={() => setHidden(true)}>
         <a
@@ -19,14 +18,13 @@ function ProjectBox(props) {
           rel="noopener noreferrer">
           <CardBody className="p-0">
             <CardImg
-              className="rounded-0"
               src={props.data.img_src}
               alt={props.data.img_alt}
             />
-            <div className={hidden && "d-none"}>
+            <div className={hidden ? "d-none" : ""}>
               <CardImgOverlay
-                className="text-center bg-white rounded-0 d-flex"
-                style={{ opacity: "0.85", flexWrap: "wrap" }}>
+                className="text-center bg-white d-flex"
+                style={{ opacity: "0.9", flexWrap: "wrap" }}>
                 <h2
                   style={{ minWidth: "100%" }}
                   className="align-self-center mx-auto text-primary">
