41d4f9a6f780348b19b82ca8284fcb041bb2305b
Rebecca Mead <beckyleemead@gmail.com>
2021-01-01 18:15:14 -0500

    add project carousel

---
+++ src/components/ProjectDisplay.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from "reactstrap";
+import projectsData from "../data/projects";
+
+function ProjectDisplay() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === projectsData.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? projectsData.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = projectsData.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.img_src}
+      >
+        <img
+          className="d-block w-100"
+          src={item.img_src}
+          alt={item.img_alt} />
+        <CarouselCaption
+          className="text-primary"
+          captionText={item.date}
+          captionHeader={item.title} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={projectsData} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+export default ProjectDisplay;
