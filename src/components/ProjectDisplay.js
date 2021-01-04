11d55740f8daf2cf4c85e93fbcd3b799cbf9c1f6
Rebecca Mead <beckyleemead@gmail.com>
2021-01-04 09:05:22 -0500

    rework carousel caption

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from "reactstrap";
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestion, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import projectsData from "../data/projects";
 
 function ProjectDisplay() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
-  const next = () => {
+  function next() {
     if (animating) return;
     const nextIndex = activeIndex === projectsData.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   }
 
-  const previous = () => {
+  function previous() {
     if (animating) return;
     const nextIndex = activeIndex === 0 ? projectsData.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
@@ -34,10 +36,15 @@ function ProjectDisplay() {
           className="d-block w-100"
           src={item.img_src}
           alt={item.img_alt} />
-        <CarouselCaption
-          className="text-primary"
-          captionText={item.date}
-          captionHeader={item.title} />
+        <div
+          className="text-primary carousel-caption d-none d-md-block">
+          <h3>{item.title}</h3>
+          <p>{item.date}</p>
+          <FontAwesomeIcon icon={faQuestion} className="m-3" />
+          <a href={item.href} target="_blank">
+            <FontAwesomeIcon icon={faExternalLinkAlt} className="m-3" />
+          </a>
+        </div>
       </CarouselItem>
     );
   });
@@ -47,6 +54,7 @@ function ProjectDisplay() {
       activeIndex={activeIndex}
       next={next}
       previous={previous}
+      className="mb-5"
     >
       <CarouselIndicators items={projectsData} activeIndex={activeIndex} onClickHandler={goToIndex} />
       {slides}
