1e8bba2d961e9fa239ad17904d365ae719861138
Rebecca Mead <beckyleemead@gmail.com>
2021-01-26 12:47:56 -0500

    update blurb

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         className="bg-primary sticky-top text-secondary p-3 m-0">
         <Container fluid>
           <h1>R. L. MEAD</h1>
-          <h5>data engineer, full stack developer, linguist, maker</h5>
         </Container>
       </Jumbotron>
       <ProjectDisplay />
@@ -24,7 +23,7 @@ function App() {
         <Row>
           <Col md={{ size: 6, offset: 3 }}>
             <h2 className="text-center">about me</h2>
-            <p>i'm a generalist with lots of interests and creative drive.</p>
+            <p>i'm a data engineer and full stack developer with lots of interests and creative drive.</p>
             <p>i love making things, and i love learning. when introduced to a new system, i enjoy thinking of ways to improve it. when possible, i take the initiative to make those improvements myself.</p>
             <p>i live for creative collaboration. i'm also inspired by sustainability, design, science, and social cooperation.</p>
           </Col>
