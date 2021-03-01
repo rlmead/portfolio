d5436402712b91507942fd0e2f26b715fd32da52
Rebecca Mead <beckyleemead@gmail.com>
2021-03-01 14:51:24 -0500

    update bio

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
         <Col md={{ size: 6, offset: 3 }}>
           <p>i'm a data engineer and full stack developer with lots of interests and creative drive.</p>
           <p>i love making things, and i love learning. when introduced to a new system, i enjoy thinking of ways to improve it. when possible, i take the initiative to make those improvements myself.</p>
-          <p>i live for creative collaboration. i'm motivated and inspired by social justice, sustainability, design, and science.</p>
+          <p>i live for creative collaboration. i'm motivated and inspired by racial equity and social justice, sustainability, design, and science.</p>
         </Col>
       </Row>
     </Container>
